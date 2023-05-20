3da45bdf51323a2e7c51dba14b26077dd0d3ebad
VeFe24 <128744051+VeFe24@users.noreply.github.com>
2023-05-20 11:27:31 +0200

    add competition signup/leave

---
+++ signup_competition.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+require_once("parts/db.php");
+
+if (!isset($_GET["competition"])) {
+    // nebol správne volaný skript
+    header("Location: index.php");
+    die();
+}
+
+$client = new Database();
+$client->signup_competition($_SESSION["id"], $_GET["competition"]);
+?>
+
+<script>
+    alert("Boli ste úspešne priadaný do súťaže.");
+    window.location.href = "competitions.php";
+</script>
